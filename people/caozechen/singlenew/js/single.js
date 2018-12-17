f13a6658e91c381fbd7fdd9f7d8747a132285562
caozechenc <m15226507930_1@163.com>
2018-12-17 18:45:23 +0800

    曹泽辰

---
+++ people/caozechen/singlenew/js/single.js
@@ -0,0 +1,206 @@
+ window.onload = function () {
+        var domAddToCart = document.getElementById('addToCart');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+
+              
+        }
+
+
+
+
+
+
+
+
+
+
+var domAddToCart = document.getElementById('addToCart1');
+        domAddToCart.onclick = function () {
+ 
+            var domAddToCart = document.getElementById('addToCart1');
+            var domShopCart = document.getElementById('shopCart');
+            domShopCart.value=parseInt(domShopCart.value)+1;
+            
+        }
+
+
+        var domAddToCart = document.getElementById('addToCart2');
+        domAddToCart.onclick = function () {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart2');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+            
+                    
+        }
+
+
+
+
+
+        var domAddToCart = document.getElementById('addToCart#');
+        domAddToCart.onclick = function () {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart#');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+           
+        }
+
+        var domAddToCart = document.getElementById('addToCart3');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart3');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+
+
+
+var domAddToCart = document.getElementById('addToCart4');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart4');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+
+
+
+var domAddToCart = document.getElementById('addToCart5');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart5');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+
+
+
+var domAddToCart = document.getElementById('addToCart6');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart6');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+               
+        }
+
+var domAddToCart = document.getElementById('addToCart7');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart7');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+
+var domAddToCart = document.getElementById('addToCart8');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart8');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+
+        var domAddToCart = document.getElementById('addToCart9');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart9');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+
+        var domAddToCart = document.getElementById('addToCart10');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart10');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+
+        var domAddToCart = document.getElementById('addToCart11');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart11');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+
+
+        var domAddToCart = document.getElementById('addToCart12');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart12');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+
+        var domAddToCart = document.getElementById('addToCart13');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart13');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+var domAddToCart = document.getElementById('addToCart14');
+        domAddToCart.onclick = function (event) {
+          // var num=document.getElementById("num");
+                
+            var domAddToCart = document.getElementById('addToCart14');
+          
+            var domShopCart = document.getElementById('shopCart'); 
+            domShopCart.value=parseInt(domShopCart.value)+1;
+                
+        }
+
+}
+
+
+
+
+
+
+
+    
