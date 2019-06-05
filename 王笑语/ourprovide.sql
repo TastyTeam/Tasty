0d7f8e6ea62e42ba44831f46b297479ecdc91139
happytalk <779143880@qq.com>
2019-06-05 08:59:12 +0800

    database

---
+++ 王笑语/ourprovide.sql
@@ -0,0 +1,3 @@
+INSERT INTO `ourprovide` VALUES (1, '美食外卖', 'images/coffee.png', '趣吃外卖以严谨认真的态度筛选审查商家，通过层层把关，选择卫生健康的可靠商家入驻，具有安全保障。');
+INSERT INTO `ourprovide` VALUES (2, '果蔬购买', 'images/dinner.png', '趣吃果蔬联合果蔬生态园，提供最权威的绿色生态认证，从种植到生产严格把关，最终呈现给顾客。');
+INSERT INTO `ourprovide` VALUES (3, '餐食定制', 'images/lunch.png', '无论是新手小白，还是家庭主妇，无论想吃西餐还是中餐，在私人订制中总能选到你想要的。');
