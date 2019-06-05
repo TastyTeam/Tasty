0c9ef24f7ee069bc1c6bf49067f01007cbb0afb0
happytalk <779143880@qq.com>
2019-06-05 08:53:01 +0800

    database update

---
+++ 王笑语/articletype.sql
@@ -0,0 +1,4 @@
+INSERT INTO `articletype` VALUES (1, '美食攻略');
+INSERT INTO `articletype` VALUES (2, '美食资讯');
+INSERT INTO `articletype` VALUES (3, '养生常识');
+INSERT INTO `articletype` VALUES (4, '餐厅新闻');
